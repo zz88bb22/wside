1c7ebc6f2f1d7e2b30298f76299d0d46986a306a
7fdsfnkd <fjdaksfj@fjdsk.com>
2018-02-21 19:40:38 -0500

    fx-frm-snd

--- main.js
+++ main.js
@@ -1,54 +1,37 @@
-$("#js-form-submit").click(function() {
-  validateForm();
-  return false;
+var $contactForm = $('#js-form-send');
+
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	var $submit = $('input:submit', $contactForm);
+  var defaultSubmitText = $submit.val();
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var offer = $("input#price").val();
+        
+
+	$.ajax({
+		url: 'https://formspree.io/zztop22@protonmail.com',
+		method: 'POST',
+		data: {
+      name: name,
+      email: email,
+      offer: offer
+    },
+		dataType: 'json',
+		beforeSend: function() {
+			$submit.attr('disabled', true).val('Sending message…');
+		},
+		success: function(data) {
+			$submit.val('Message sent! We will be in touch.');
+			//setTimeout(function() {
+			//	$submit.attr('disabled', false).val(defaultSubmitText);
+			//}, 4000);
+		},
+		error: function(err) {
+			$submit.val('There was an error. Try again.');
+			setTimeout(function() {
+				$submit.attr('disabled', false).val(defaultSubmitText);
+			}, 4000);
+		}
+	});
 });
-function validateForm() {
-  var name, email, price, atpos, dotpos;
-  name = $("#name").val();
-  email = $("#email").val();
-  price = $("#price").val();
-  atpos = email.indexOf("@");
-  dotpos = email.lastIndexOf(".");
-  if (name == null || name == "") {
-    $("#js-form-message")
-      .addClass("text-error text-large")
-      .text("* Please enter your name.");
-    $("#name").focus();
-    return false;
-  }
-  if (email == null || email == "") {
-    $("#js-form-message")
-      .addClass("text-error text-large")
-      .text("* Please enter your email address.");
-    $("#email").focus();
-    return false;
-  }
-  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
-    $("#js-form-message")
-      .addClass("text-error text-large")
-      .text("* Please enter a valid email address.");
-    $("#email").focus();
-    return false;
-  }
-  if (price == null || price == "") {
-    $("#js-form-message")
-      .addClass("text-error text-large")
-      .text("* Please an offer price in U.S. Dollars.");
-    $("#price").focus();
-    return false;
-  } else {
-    var datastring = $("#js-form-send").serialize();
-    $.ajax({
-      url: "https://formspree.io/zztop22@protonmail.com",
-      method: "POST",
-      data: datastring,
-      dataType: "json"
-    });
-    $("#js-form-message")
-      .addClass("text-success")
-      .text(
-        "Your inqury has been sent successfully. We will be in touch as soon as possible. Thank you for your interest."
-      );
-    return false;
-  }
-}
